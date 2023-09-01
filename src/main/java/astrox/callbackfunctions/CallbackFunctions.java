9f3755a62ec8108a546de9efe42fccd76fba0d10
BalazsDev <nabali87@gmail.com>
2023-09-01 20:26:16 +0200

    Animation to toggleFullScreen

--- src/main/java/astrox/callbackfunctions/CallbackFunctions.java
+++ src/main/java/astrox/callbackfunctions/CallbackFunctions.java
@@ -4,12 +4,13 @@ import astrox.mainframe.MainFrame;
 import org.cef.CefApp;
 
 import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CallbackFunctions {
 
     private MainFrame astro;
-    private boolean isFullScreen = false;
-    private Dimension originalSize;
 
     public CallbackFunctions(MainFrame astro) {
         this.astro = astro;
@@ -45,6 +46,65 @@ public class CallbackFunctions {
         } catch (Exception exception) {
             System.out.println("Error exit: " + exception);
         }
+    }
+
+    private boolean isFullScreen = false;
+    private Dimension originalSize;
+    private Point originalLocation;
+    private Timer timer;
+    private long startTime = 100;
+    private int animationDuration = 1000; // Duration of the animation in milliseconds
+    private int animationStep = 20; // Time interval for animation updates
+    private int initialWidth = 800; // Initial window width
+    private int initialHeight = 600; // Initial window height
+    private int targetWidth;
+    private int targetHeight;
+    private int currentWidth = initialWidth;
+    private int currentHeight = initialHeight;
+
+    public void FullScreenAnimation() {
+
+        originalSize = astro.mainFrame.getSize();
+        originalLocation = astro.mainFrame.getLocation();
+
+        targetWidth = Toolkit.getDefaultToolkit().getScreenSize().width; // Full-screen width
+        targetHeight = Toolkit.getDefaultToolkit().getScreenSize().height; // Full-screen height
+
+        timer = new Timer(animationStep, new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (startTime == 0) {
+                    startTime = System.currentTimeMillis();
+                }
+
+                long currentTime = System.currentTimeMillis();
+                long elapsedTime = currentTime - startTime;
+
+                if (elapsedTime >= animationDuration) {
+                    // Animation complete
+                    timer.stop();
+                    astro.mainFrame.setSize(targetWidth, targetHeight);
+                    astro.mainFrame.setLocation(0, 0);
+                    astro.mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                } else {
+                    // Calculate the new window size based on the elapsed time
+                    double progress = (double) elapsedTime / animationDuration;
+                    int newWidth = (int) (initialWidth + progress * (targetWidth - initialWidth));
+                    int newHeight = (int) (initialHeight + progress * (targetHeight - initialHeight));
+                    astro.mainFrame.setSize(newWidth, newHeight);
+
+                    // Center the window during the animation
+                    int x = (targetWidth - newWidth) / 2;
+                    int y = (targetHeight - newHeight) / 2;
+                    astro.mainFrame.setLocation(x, y);
+                }
+            }
+        });
+    }
+
+    public void startAnimation() {
+        timer.start();
     }
 
     public void toggleFullscreen() {
